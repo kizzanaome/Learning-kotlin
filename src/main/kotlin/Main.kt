064d572fbc7185c038c4ca30df49afbada4463c8
kizzanaome <naomekizzan@gmail.com>
2023-07-23 15:20:55 +0300

    Introduction to kotlin

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,20 @@
+fun main(){
+
+//    Variables
+
+//    println("Enter year")
+//        val year = readln().toInt()
+//        when(year){
+//            in 0..1959 ->println("Gen Z")
+//            in 1945..1959->println(" Milleanial")
+//            in 1959..2000 ->println("Gen x")
+//            else-> println("Boby Boomer")
+//        }
+
+    //    println("The sum of two values is $sum")
+    val flo4:Float =.3f
+    print(flo4)
+
+
+
+    }
