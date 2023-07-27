ff06c3546dd843be52966712437c1cab6d67654b
kizzanaome <naomekizzan@gmail.com>
2023-07-27 17:23:12 +0300

    Functions assignment

--- src/main/kotlin/Functions assignment.kt
+++ src/main/kotlin/Functions assignment.kt
@@ -1,9 +1,6 @@
 fun main(){
-//    lambdas are simplified functions or an anonymous function
-//    or a higher order function
-//    val add ={a:Int -> println("The value is $a") }
-//    add(6)
-    println("Welcome to Noma calculator")
+
+    println("Welcome to Noma's calculator")
 
     println("Please input integer value ")
     val value1:Double = readln().toDouble()
@@ -14,19 +11,11 @@ fun main(){
     println("* + - /")
 
     val character = readln().toString()
-    calculate(value1,value2,character)
+    calculator(value1,value2,character)
 
 
 }
 
-
-
-fun addTwoNum(a:Int,b:Int):Int{
-    val x = a+b
-    print("The sum of $a and $b is: $x")
-    return  x
-
-}
 
 //write a function that has three parameters; two doubles and one character
 // (*,+,-,/) and checks if third parameter == *, it will multiply them.
@@ -34,9 +23,9 @@ fun addTwoNum(a:Int,b:Int):Int{
 // == -, it will subtract the two numbers
 //== /, it will divide the two numbers
 
-fun calculate(value1:Double, value2:Double, character:String): Double {
+fun calculator(value1:Double, value2:Double, character:String): Double {
     if (character=="*"){
-       val answer = (value1 * value2).toInt()
+        val answer = (value1 * value2).toInt()
         println("The product of $value1 and $value2 = $answer")
         return answer.toDouble()
     }else if(character=="+"){
